14216c5d248380adb6e61dfd6d54d7b28a05cd83
Mutsuki <yukkeca@outlook.jp>
2023-11-19 00:23:11 +0900

    - 敵陣の札の表示を上下左右反転 - コメントを追加

--- src/web/components/CardField.tsx
+++ src/web/components/CardField.tsx
@@ -16,15 +16,19 @@ const sides = ['敵陣', '自陣'];
 // DroppableZone.tsx
 export const CardField = (props: any) => {
     const { id, key } = props;
+    const enemySideClass = " scale-x-[-1] scale-y-[-1]";
     
     return (
         <>
+            {/* 相手側 or 自分側 */}
             {sides.map((side) => (
               <div key={side} className="flex gap-8 m-4 justify-center">
+                  {/* 左側 or 右側 */}
                   {lrZones.map((lrZone) => (
                       <div key={lrZone}>
+                          {/* 上中下 */}
                           {rowZones.map((rowZone) => (
-                            <section className={`game-field mb-4`} key={key} id={`${side}-${lrZone}-${rowZone}`}>
+                            <section className={`game-field mb-4` + (side == "敵陣" ? enemySideClass : "")} key={key} id={`${side}-${lrZone}-${rowZone}`}>
                                 {(() => {
                                     const fields = [];
                                     for (let i = 0; i < 8; i++) {
